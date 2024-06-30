d52be6cc940461ca227dabb32b8e7446a6b3eb72
abi fathan yusuf <abiefathan333@gmail.com>
2024-07-01 03:42:47 +0700

    [INISIALISASI BARU] kasir sederhana

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import React from "react"
+import NavbarComponent from "./components/NavbarComponent"
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+       
+     <NavbarComponent/>
+
+    </>
+  )
+}
+
+export default App
